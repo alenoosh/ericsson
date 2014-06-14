b89d10a588e7cf5818ce34faa105ab6156d98f3b
alenoosh <hope2end@gmail.com>
2014-06-14 12:23:29 +0430

    New code changes are added.

---
+++ www/public/ericsson/dbConfig.php
@@ -0,0 +1,8 @@
+<?php
+
+// MySql Connection info
+
+define('ERICSSON_SERVER', 'localhost');
+define('ERICSSON_MYSQL_USER', 'root');
+define('ERICSSON_MYSQL_PASS', 'root');
+define('ERICSSON_MYSQL_DB', 'ericsson_test');
